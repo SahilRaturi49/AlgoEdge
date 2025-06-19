59f42ab323880f73c86230c258e9be50717aa1b8
sahil <sahilraturi49@gmail.com>
2025-06-19 17:00:21 +0530

    feat: add problem routes with authentication middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,7 +23,9 @@ app.get("/", (req, res) => {
 
 // routes
 import authRoutes from "./routes/auth.routes.js";
+import problemRoutes from "./routes/problem.routes.js";
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/problems", problemRoutes);
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on port: ${PORT}`);
