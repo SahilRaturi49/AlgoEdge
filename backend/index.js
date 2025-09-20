f7aec69f6b5ef68f0bb04d3dcc2956bb8f254ea8
sahil <sahilraturi49@gmail.com>
2025-09-20 22:06:28 +0530

    fix: some minor changes

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://algo-edge.vercel.app/"],
+    origin: "https://algo-edge.vercel.app",
     credentials: true,
   })
 );
