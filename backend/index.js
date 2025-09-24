32b932664bcdab2e66371116fb6d5e8f40491440
sahil <sahilraturi49@gmail.com>
2025-09-24 11:08:56 +0530

    fix: update CORS configuration to use dynamic allowed origins

--- backend/index.js
+++ backend/index.js
@@ -9,12 +9,25 @@ dotenv.config({
   path: process.env.NODE_ENV === "production" ? ".env.production" : ".env",
 });
 
+// app.use(
+//   cors({
+//     origin: "https://algo-edge.vercel.app",
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
+
+const allowedOrigins = process.env.FRONTEND_ORIGINS.split(",");
 app.use(
   cors({
-    origin: "https://algo-edge.vercel.app",
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1)
+        return callback(new Error("CORS not allowed"), false);
+      return callback(null, true);
+    },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
